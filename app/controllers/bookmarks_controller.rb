e88abc3c1809838dbfba0a04e7eae7d35ecfcd73
Lood Rabie <lood0518@gmail.com>
2024-05-28 21:12:01 +0200

    able to bookmark movies

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,26 @@
 class BookmarksController < ApplicationController
+
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
     @movies = Movie.all
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.build(bookmark_params)
+
+    if @bookmark.save
+      redirect_to list_path(@list), notice: 'Bookmark was created.'
+    else
+      @movies = Movie.all
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
